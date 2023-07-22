79eb4889ec7c606673760af0bf5b6b1d5b944a38
vivekraina2023 <vivekraina2023@gmail.com>
2023-07-22 15:58:02 -0400

    initial base version

---
+++ src/test/java/com/example/demo3_configserver/Demo3ConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo3_configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo3ConfigserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
